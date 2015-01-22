8488de685bcd3c1122bde9b1488f7b32f7db2239
AZARI Nicolas <nicodinh@azari.fr>
2015-01-22 08:50:44 +0100

    First commit

---
+++ server/lib/faw-player.js
@@ -0,0 +1,14 @@
+//
+// classe de definition d'un joueur
+//
+
+function FawPlayer(name, websocketId) {
+    this.name = name;
+    this.websocketId = websocketId;
+}
+
+FawPlayer.prototype = {
+
+};
+
+module.exports = FawPlayer;
